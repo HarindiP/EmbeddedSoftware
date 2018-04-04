145c0f466091bf6bf134c1348940c3bd24bfa27c
Danon Bradford <danon.bradford@uts.edu.au>
2018-04-04 06:42:05 +0000

    Update packet.c

--- Lab1/Sources/packet.c
+++ Lab1/Sources/packet.c
@@ -88,6 +88,7 @@ bool Packet_Get(void)
  *  @return bool - TRUE if a valid packet was sent.
  */
 bool Packet_Put(const uint8_t command, const uint8_t parameter1, const uint8_t parameter2, const uint8_t parameter3){
+    // Stop if one of these does not work!
   bool b = UART_OutChar(command) && UART_OutChar(parameter1) && UART_OutChar(parameter2) && UART_OutChar(parameter3);
   return (b && UART_OutChar(command^parameter1^parameter2^parameter3));			//finally, calculate and put the checkSum
 }
