aca6a75a46f70ad5699be4875930943d96335678
13115605 <13115605@LAB1111403006.adsroot.uts.edu.au>
2018-05-25 13:52:16 +1000

    Adding a new folder to start on Lab 5 with RTOS

---
+++ OSExample/Debug/Generated_Code/Vectors.d
@@ -0,0 +1,15 @@
+Generated_Code/Vectors.o: ../Generated_Code/Vectors.c \
+ ../Generated_Code/Cpu.h ../Generated_Code/PE_Types.h \
+ ../Generated_Code/IO_Map.h \
+ C:/Users/13115605/Downloads/OSExample/OSExample/Static_Code/IO_Map/MK70F12.h \
+ C:\Users\13115605\Downloads\OSExample\OSExample\Library/OS.h
+
+../Generated_Code/Cpu.h:
+
+../Generated_Code/PE_Types.h:
+
+../Generated_Code/IO_Map.h:
+
+C:/Users/13115605/Downloads/OSExample/OSExample/Static_Code/IO_Map/MK70F12.h:
+
+C:\Users\13115605\Downloads\OSExample\OSExample\Library/OS.h:
---
+++ OSExample/Debug/Sources/main.d
@@ -0,0 +1,16 @@
+Sources/main.o: ../Sources/main.c \
+ C:/Users/13115605/Downloads/OSExample/OSExample/Generated_Code/Cpu.h \
+ C:/Users/13115605/Downloads/OSExample/OSExample/Generated_Code/PE_Types.h \
+ C:/Users/13115605/Downloads/OSExample/OSExample/Generated_Code/IO_Map.h \
+ C:/Users/13115605/Downloads/OSExample/OSExample/Static_Code/IO_Map/MK70F12.h \
+ C:\Users\13115605\Downloads\OSExample\OSExample\Library/OS.h
+
+C:/Users/13115605/Downloads/OSExample/OSExample/Generated_Code/Cpu.h:
+
+C:/Users/13115605/Downloads/OSExample/OSExample/Generated_Code/PE_Types.h:
+
+C:/Users/13115605/Downloads/OSExample/OSExample/Generated_Code/IO_Map.h:
+
+C:/Users/13115605/Downloads/OSExample/OSExample/Static_Code/IO_Map/MK70F12.h:
+
+C:\Users\13115605\Downloads\OSExample\OSExample\Library/OS.h:
